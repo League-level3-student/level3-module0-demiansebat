93fd2cdec4b12d175a175f0731d317c689c95c1d
league <league@imac-12.attlocal.net>
2022-10-06 18:27:52 -0700

    starting firework display

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -4,8 +4,16 @@ import java.util.Random;
 
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
+public static void main(String[] args) {
+	
+}
 
-
+public static void allString(String[] str) {
+	for (int i = 0; i < str.length; i++) {
+		System.out.println(str[i]);
+		
+	}
+}
 
     //2. Write a method that takes an array of Strings and prints all the Strings in the array.
 
@@ -13,14 +21,29 @@ public class MoreArrayFun {
 
     //3. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in reverse order.
-
-
+public static void backward(String[] str) {
+for (int i =str.length-1; i>=0; i--) {
+	System.out.println(str[i]);
+}
+}
 
     //4. Write a method that takes an array of Strings and prints every other String in the array.
-
-
+public static void second(String[] str) {
+	for (int i = 0; i < str.length; i++) {
+if (i%2==0) {
+	System.out.println(str[i]);
+}
+		}
+	}
+	
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
+public static void randy(String[] str) {
+Random hmmm= new Random();
+for (int i = 0; i < str.length; i++) {
+	i=hmmm.nextInt(str.length);
+}
 
+}
 
 }
--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -23,15 +23,21 @@ for (int i = 0; i < values.length; i++) {
     //   array contains the value specified by the second parameter.
     //   It should otherwise return false.
     public static boolean containsIntValue(int[] array, int value) {
-
-        return false;
+    	for (int i = 0; i < array.length; i++) {
+			if (array[i]==value) {
+				return true;
+			}
+		}
+		return false;
     }
 
     //4. Complete the method so that it returns the index of the,
     //   first instance that the specified value occurs in the array.
     //   If the array does not contain the specified value, it should return -1.
     public static int getIndex(int[] arr, int value) {
-
+  for (int i = 0; i < arr.length; i++) {
+	return i;
+}
         return 0;
     }
 }
